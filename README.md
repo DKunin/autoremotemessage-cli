96dade3dfbeebbbe1ef746866513d4c029eb7a30
Dmitry Kunin <dkunin@avito.ru>
2015-10-07 14:52:34 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Description
+Simple cli for using with autoremotemessage module
+
+## Usage
+      
+      node index.js --title Title --text Textmessage
+      node index.js -t Title -m Textmessage
+
+Or after setting up link
+      
+      aurm --title Title --text Textmessage
+      aurm -t Title -m Textmessage
+      aurm --title Title --text Textmessage --key YOUR_API_KEY
+
+      aurm --config apikey=<YOUR_API_KEY>
---
+++ index.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var arnotif              = require('autoremotenotification');
+var argv                 = require('yargs').argv;
+var defaultNotif         = {message: 'Message'};
+var R                    = require('ramda');
+var nconf                = require('nconf');
+var path                 = require('path');
+var autoremoteConfigFile = path.join(__dirname, 'autoremotekey.json');
+ 
+nconf.argv()
+     .env()
+     .file({ file: autoremoteConfigFile });
+
+var apiKey = nconf.get('apikey');
+var config = nconf.get('config');
+
+if(config) {
+  var data = config.split('=');
+  nconf.set(data[0], data[1]);
+  nconf.save(function (err) {
+    if(err) {
+      throw new Error(err);
+    }
+  });
+  console.log('settings updated');
+  return;
+}
+
+if(argv.m) {
+  argv.text = argv.m;
+}
+if(argv.t) {
+  argv.title = argv.t;
+}
+
+argv.key = apiKey;
+
+
+arnotif(R.merge(defaultNotif, argv));
